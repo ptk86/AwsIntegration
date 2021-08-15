08642576520e2c3e996cb8d5c7504b04ddfb1917
Piotr Kiliński <piotr.kilinski@gmail.com>
2021-08-15 20:54:53 +0200

    Add Streaming Api project

---
+++ Streaming.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Streaming.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StreamingController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Get()
+        {
+            return Accepted();
+        }
+    }
+}
